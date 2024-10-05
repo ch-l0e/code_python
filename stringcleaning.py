b32251f774813d8c3151b4ecc684b585d67f0e73
ch-l0e <chloechan2201@gmail.com>
2024-10-05 17:21:56 -0400

    created 1 file

--- stringcleaning.py
+++ stringcleaning.py
@@ -1,8 +1,16 @@
-user_input = input("Enter a string: ")
-clean_string = ""
-for i in user_input:
-    if (ord(i) >= 65 and ord(i) <= 90 or ord(i) >= 97 and ord(i) <= 122):
-        clean_string = clean_string + i
-    #print(ord(i))
+def string_cleaner(prompt):
+    user_input = input(prompt)
+    clean_string = ""
+    for i in user_input:
+        if i.isalpha():
+            clean_string = clean_string + i
+    return clean_string # only in function
 
-print(clean_string)
+# user_input = input("Enter a string: ")
+# clean_string = ""
+# for i in user_input:
+#     if (ord(i) >= 65 and ord(i) <= 90 or ord(i) >= 97 and ord(i) <= 122):
+#         clean_string = clean_string + i
+#     #print(ord(i))
+
+print(string_cleaner("Enter a string: "))
