384f2b73e52e2f6425c8a86bdc9e85b5ba1d3a00
ch-l0e <chloechan2201@gmail.com>
2024-10-06 11:02:30 -0400

    created 1 file

--- consonantcounter.py
+++ consonantcounter.py
@@ -1,11 +1,30 @@
 def consonantCounter(prompt):
-    vowels = "bcdfghjklmnpqrstvwxyz"
+    consonants = "bcdfghjklmnpqrstvwxyz"
     counter = 0
-    user_input = input(prompt)
+    user_input = input(prompt).lower()
+    for i in user_input:
+        if consonants.find(i) != -1:
+            counter += 1
+    return counter
+
+def vowelCounter(prompt):
+    vowels = "aeiou"
+    counter = 0
+    user_input = input(prompt).lower()
     for i in user_input:
         if vowels.find(i) != -1:
             counter += 1
     return counter
 
-num_consonant = consonantCounter("Enter a string: ")
-print(f"The number of consonants are: {num_consonant}")
+while True:
+    user_choice = input("Enter 'c' to pick consonants and 'v' to pick vowels: ").lower()
+    if user_choice == "c":
+        num_consonant = consonantCounter("Enter a string: ")
+        print(f"The number of consonants in the string is: {num_consonant}")
+        break
+    elif user_choice == "v":
+        num_vowel = vowelCounter("Enter a string: ")
+        print(f"The number of consonants in the string is: {num_vowel}")
+        break
+    else:
+        print("Invalid input. Please try again.")
